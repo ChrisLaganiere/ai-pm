08b33f9c9a88c78b3f0f74978ce32f010ca3f21d
Chris Laganiere <chrislaganiere@gmail.com>
2025-07-04 23:43:11 -0700

    Set up goofy cartoon

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import './theme.css';
 import { KanbanBoardView } from './Components/KanbanBoard';
 import { demoFixtures } from './Fixtures/demoFixtures';
 import { formatBoard, KanbanColumn } from "./Models/KanbanModels";
+import './theme.css';
+import styles from './App.module.css';
+import stalin from './Resources/stalin-pm.png';
 
 function App() {
   const [items, setItems] = useState(demoFixtures())
@@ -17,7 +19,15 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className={`App ${styles.KanbanHeaderView}`}>
+      <div className={styles.aiPm}>
+        <div className={styles.aiPmCartoon}>
+          <img src={stalin} alt="ai pm cartoon" />
+        </div>
+        <div className={styles.aiPmSpeech}>
+          Well, is that the best you can do?
+        </div>
+      </div>
       <KanbanBoardView board={formatBoard(items)} moveItem={moveItem} />
     </div>
   );
